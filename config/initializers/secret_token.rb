2efba20fe6444a6dd9c47df22ee2f7a56673ab95
MJ Meyer <mj@paydirect.co.za>
2011-01-27 19:17:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '218dc9d1354f72658cc74b32eaca94f2cc2e613cde99e41ca6108ebc17e16c8b8bcc211a47e69ac1cdcf7307f39d9e9ecdbe93627169728c90bae0aa34559ec8'
